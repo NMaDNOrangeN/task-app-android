7193637773a956c9d3712f74f99055a62d8b6504
Orange <caesgou@mail.ru>
2026-09-15 17:07:58 +0500

    Сделал задание 1

--- app/src/main/java/com/example/task_app_android/MainActivity.java
+++ app/src/main/java/com/example/task_app_android/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.task_app_android;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,45 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView studentData = findViewById(R.id.textView1);
+        TextView groupData = findViewById(R.id.textView2);
+        ImageView imageView = findViewById(R.id.imageView);
+        Button buttonHideInfo = findViewById(R.id.button);
+        ImageButton imageButtonHide = findViewById(R.id.imageButton1);
+
+        studentData.setText("Shabalin E.K.");
+        studentData.setTextSize(20);
+        studentData.setTextColor(Color.GRAY);
+
+        groupData.setText("T-433901-IST");
+        groupData.setTextSize(20);
+        groupData.setTextColor(Color.RED);
+
+        imageView.setImageResource(R.drawable.btr);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        buttonHideInfo.setText("Hide INFO");
+        buttonHideInfo.setTextSize(20);
+        buttonHideInfo.setOnClickListener(v -> {
+            if (studentData.getVisibility() == TextView.VISIBLE) {
+                studentData.setVisibility(TextView.INVISIBLE);
+                groupData.setVisibility(TextView.INVISIBLE);
+            }
+            else {
+                studentData.setVisibility(TextView.VISIBLE);
+                groupData.setVisibility(TextView.VISIBLE);
+            }
+        });
+
+        imageButtonHide.setImageResource(R.drawable.inv);
+        imageButtonHide.setScaleType(ImageButton.ScaleType.CENTER_INSIDE);
+        imageButtonHide.setOnClickListener(v -> {
+            if (imageView.getVisibility() == ImageView.VISIBLE)
+                imageView.setVisibility(ImageView.INVISIBLE);
+            else
+                imageView.setVisibility(ImageView.VISIBLE);
         });
     }
 }
